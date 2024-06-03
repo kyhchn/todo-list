212a637109a9f9ee5cd8d4c54d1c9c0aaa0a4bd4
kyhchn <alvinkn.dev@gmail.com>
2024-06-03 08:01:51 +0700

    feat(nextauth): fix bug

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/lib/authoptions";
 import NextAuth from "next-auth";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
